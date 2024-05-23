446d117d072f985d38c3062b6436471fe519bb52
Sooyoung <agriades23@gmail.com>
2024-05-23 21:10:51 +0900

    feat: Variables.input으로 singleton으로 선언한 input을 가져올 수 있다.

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -9,19 +9,18 @@ import com.sparta.lifecycle.databinding.ActivityMainBinding
 // onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-    var input: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.et.setText(input) //이렇게 선언하니 에러가 떴다! 냅다 "hello" 했을 땐 괜찮았는데.
+        binding.et.setText(Variables.input) //이렇게 선언하니 에러가 떴다! 냅다 "hello" 했을 땐 괜찮았는데.
     //출력 시에는 binding.et.text가 가능하지만, 반대는 안된다.
     }
 
     override fun onStop() {
             super.onStop()
-            input = binding.et.text.toString()
-            Log.d("agriades", input)
+            Variables.input = binding.et.text.toString()
+            Log.d("agriades", Variables.input)
         }
 
 }
