385671c7b92a7694710f785e63cb9f745f66726b
Sooyoung <agriades23@gmail.com>
2024-05-23 20:58:58 +0900

    docs: 할 일 주석 처리함.

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -10,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        //어딘가에 저장되어 있던 binding.et.text 정보를 여기에서 binding.et.text에 다시 집어넣음.
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //여기에 바깥으로 입력값 저장하는 코드 작성
     }
 }
