3703ced0f6b55b4d83e980e95cc21f43ee112c74
Sooyoung <agriades23@gmail.com>
2024-05-23 17:45:48 +0900

    error: 앱 실행 오류로 findViewById를 주석 처리했다.
    576-1740 TaskPersister system_server
    E File error accessing recents directory (directory doesn't exist?).

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.sparta.lifecycle
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 // onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
 
-    val et = findViewById<EditText>(R.id.et)
+//    val et = findViewById<EditText>(R.id.et)
     lateinit var input: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        input = et.text.toString()
+        input = "hello!" //et.text.toString()
+        Log.d("agriades", input)
     }
 
 }
