5dc13c13142cb2945fdc8f9735ef577f16780d39
Sooyoung <agriades23@gmail.com>
2024-05-23 23:34:55 +0900

    feat: saveTextInputScope와 saveTextInput 함수 2개를 선언! 하면 비교할 수 있겠지?

--- app/src/main/java/com/sparta/lifecycle/MainViewModel.kt
+++ app/src/main/java/com/sparta/lifecycle/MainViewModel.kt
@@ -3,12 +3,21 @@ package com.sparta.lifecycle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 
 class MainViewModel: ViewModel() {
     private val _textInput: MutableLiveData<String> = MutableLiveData() //LiveData와 MVVM은 친구 친구!
     val textInput: LiveData<String> get() = _textInput
 
+    fun saveTextInputScope(text: String){
+        viewModelScope.launch{
+            _textInput.postValue(text)
+        }
+    }
+
     fun saveTextInput(text: String){
-        _textInput.value = text
+        _textInput.postValue(text)
     }
 }
