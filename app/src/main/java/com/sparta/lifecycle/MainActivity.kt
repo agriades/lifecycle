c646c905181c936d2332ad3c50d331c6f39d437e
Sooyoung <agriades23@gmail.com>
2024-05-23 17:47:20 +0900

    refactor: 엥! 전에 본게 틀렸네. findViewById는 onCreate에 써야 했다!!!

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -8,12 +8,12 @@ import androidx.appcompat.app.AppCompatActivity
 // onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
 
-//    val et = findViewById<EditText>(R.id.et)
     lateinit var input: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val et = findViewById<EditText>(R.id.et)
     }
 
     override fun onStop() {
