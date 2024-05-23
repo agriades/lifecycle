b5fc4097aaa89fefbb83b4e6b48e35d8983cca61
Sooyoung <agriades23@gmail.com>
2024-05-23 22:03:25 +0900

    feat: 빈 viewModel 생성해 메인에 연결함.
    뷰모델의 단독 역할: UI를 위한 데이터 관리 뿐! 뷰모델은 연동된 프래그먼트나 액티비티와 일대일 대응하는 것 같다. 앗, 근데 activity 꺼져도 뷰모델에서 정보 관리하는 거 아니었어? DB가 따로 있어서 viewModel은 주차 못하는 고속도로인 건가?
    
    참고 자료: https://developer.android.com/reference/androidx/lifecycle/ViewModel
    
    A ViewModel is always created in association with a scope (a fragment or an activity) and will be retained as long as the scope is alive.
    E.g. if it is an Activity, until it is finished.
    
    The purpose of the ViewModel is to acquire and keep the information that is necessary for an Activity or a Fragment. ViewModels usually expose this information via androidx.lifecycle.LiveData or Android Data Binding.
    
    ViewModel's only responsibility is to manage the data for the UI. It should never access your view hierarchy or hold a reference back to the Activity or the Fragment.

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -3,17 +3,23 @@ package com.sparta.lifecycle
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.sparta.lifecycle.databinding.ActivityMainBinding
 
 // onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
+//    val viewModel = ViewModelProvider(this)[MainViewModel::class.java] 가 정석 방식.
+//    val viewModel = MainViewModel by viewModels() by viewModels는 build.gradle에 라이브러리 디펜던시를 추가해야 함.
+    val viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private var input = Variables.input
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.et.setText(input) //이렇게 선언하니 에러가 떴다! 냅다 "hello" 했을 땐 괜찮았는데.
-    //출력 시에는 binding.et.text가 가능하지만, 반대는 안된다.
+        binding.et.setText(input)//출력 시에는 binding.et.text가 가능하지만, 반대는 안된다.
+
+        //ViewModel 적용 구간
     }
 
     override fun onStop() {
