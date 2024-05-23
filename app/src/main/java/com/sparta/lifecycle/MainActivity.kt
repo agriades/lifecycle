26343ce3359be4985381bd4475fddb8a922362fa
Sooyoung <agriades23@gmail.com>
2024-05-23 16:21:13 +0900

    feat: 6가지 핵심 라이프사이클의 override 틀 작성

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -3,9 +3,31 @@ package com.sparta.lifecycle
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
+// onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
