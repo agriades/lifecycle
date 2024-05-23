f53fe9e6c8610030c1dd59f8584c69e7d5de346c
Sooyoung <agriades23@gmail.com>
2024-05-23 17:55:07 +0900

    refactor: lateinit var et를 class 바로 아래에 쓸려고 해도, 타입인 EditText를 명시해 줘야 한다. 비실용적 접근.

--- app/src/main/java/com/sparta/lifecycle/MainActivity.kt
+++ app/src/main/java/com/sparta/lifecycle/MainActivity.kt
@@ -2,18 +2,20 @@ package com.sparta.lifecycle
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 // onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy()
 class MainActivity : AppCompatActivity() {
 
+    lateinit var et: EditText
     lateinit var input: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val et = findViewById<EditText>(R.id.et)
+        et = findViewById<EditText>(R.id.et)
         input = et.text.toString() //et.text.toString()
     }
 
